8bf311af73fd05bb84c0b6f27171ac0780ddffe9
anukaal <mailanu98@gmail.com>
2020-04-20 20:31:32 +0530

    Update php script

--- Login/process.php
+++ Login/process.php
@@ -4,14 +4,22 @@
     $password = $_POST['pass'];
 
 
+    // to prevent mysql injection
+
+  
     //connect to the server ans select database
-    mysql_connect("localhost", "root", "");
-    mysql_select_db("login");
+    $host = "127.0.0.1";
+    $username = "root";
+    $pass = "anurag1999";
+    $con = mysqli_connect($host, $username, $pass, "login");
+
+   
+    die(mysqli_error($con));
 
     //Query the database for user
-    $result = mysql_query("Select * from users where username = '$username' and password = '$password'")
-                or die("Failed to query database ".mysql_error());
-    $row = mysql_fetch_array($result);
+    $result = mysqli_query("Select * from users where username = '$username' and password = '$password'")
+                or die("Failed to query database ".mysqli_error());
+    $row = mysqli_fetch_array($result);
     if ($row['username'] == $username && $row['password'] == $password){
         echo "Login Success!!! Welcome Guys ",$row['username']; 
     } else {
