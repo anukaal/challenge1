f59da651ad2dbb3ae6a9ad963300ed11fcb14a43
anukaal <mailanu98@gmail.com>
2020-08-13 17:27:30 +0530

    response text

--- Automation/boolean_based.py
+++ Automation/boolean_based.py
@@ -4,3 +4,7 @@ import re
 def find_request(payload):
     response_text = requests.get(payload)
     return response_text
+
+def formatting_text(getting_response_text):
+    response_formatting_text = re.sub(r"[\n\t\s]*", "", getting_response_text.text)
+    return response_formatting_text
