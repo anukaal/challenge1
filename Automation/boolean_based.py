2b5f5524af26fe168f2a40285b5cf19cff30bee1
anukaal <mailanu98@gmail.com>
2020-08-14 07:29:01 +0530

    username length

--- Automation/boolean_based.py
+++ Automation/boolean_based.py
@@ -8,3 +8,15 @@ def find_request(payload):
 def formatting_text(getting_response_text):
     response_formatting_text = re.sub(r"[\n\t\s]*", "", getting_response_text.text)
     return response_formatting_text
+
+def username_length(payload , validator):
+    response = False
+    i = 1
+    while(response = False):
+        payload_list = list(payload)
+        payload_list[100] = str(i)
+        first_payload = payload_list
+        print(first_payload)
+
+        
+    
